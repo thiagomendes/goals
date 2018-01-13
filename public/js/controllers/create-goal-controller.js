b1a592ca722b8dcd406257c9b964031eec05bda8
mail.thiagomendes@gmail.com <mail.thiagomendes@gmail.com>
2018-01-13 18:10:03 -0200

    Integrating create goal page with API

--- public/js/controllers/create-goal-controller.js
+++ public/js/controllers/create-goal-controller.js
@@ -1,9 +1,28 @@
 angular.module('goals')
-    .controller('CreateGoalController', function ($scope, $location) {
+    .controller('CreateGoalController', function ($scope, $location, $http) {
 
         $scope.goal = {};
+        $scope.message = '';
+        $scope.isSuccess = false;
+        $scope.isError = false;
 
         $scope.cancel = function () {
             $location.path('/app/home');
+        };
+
+        $scope.submit = function () {
+
+            $scope.goal.totalCost = $scope.goal.totalCost.replace('.', '');
+            $scope.goal.totalCost = $scope.goal.totalCost.replace(',', '.');
+            $scope.goal.createDate = new Date();
+
+            $http.post('/api/goals', $scope.goal).success(function (response) {
+                $scope.message = "Successfully created goal";
+                $scope.isSuccess = true;
+                $scope.goal = response;
+            }).error(function (error) {
+                $scope.message = 'An error was encountered while creating the goal (' + error.status + ':' + error.statusText + ')';
+                $scope.isError = true;
+            });
         };
     });
